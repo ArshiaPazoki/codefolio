7f9e9b89140f969eacee21f621a305ba33c75d91
arshiapazoki <arshiapazoki2000@gmail.com>
2025-05-03 04:23:41 +0330

    feat: Minor Changes : StatusBar : Mobile Friendly

--- src/widgets/TitleBar/Titlebar.tsx
+++ src/widgets/TitleBar/Titlebar.tsx
@@ -17,7 +17,7 @@ const menuItems = [
 ]
 
 const Titlebar: FC = () => (
-  <header className="w-full flex items-center justify-between h-8 bg-[#1e1e1e] text-[#cccccc] select-none px-3 box-border ">
+  <header className="w-full flex items-center justify-between h-8 bg-[#1e1e1e] text-[#cccccc] select-none px-1.5 box-border ">
     {/* Left: VSCode icon + menu */}
     <div className="flex items-center gap-2">
       <Image
@@ -37,7 +37,7 @@ const Titlebar: FC = () => (
     </div>
 
     {/* Center: Search input */}
-    <div className="flex items-center flex-1 mx-8">
+    <div className="flex items-center flex-1 mx-6">
       <div className="relative w-full max-w-xl rounded border border-transparent hover:border-[#007acc] transition-colors duration-1000">
         <Search size={14} className="absolute left-2 top-1/2 -translate-y-1/2 text-[#888888] hover:text-[#404040]" />
         <input
@@ -51,11 +51,11 @@ const Titlebar: FC = () => (
     {/* Right: Profile & view controls + window buttons */}
     <div className="flex items-center gap-2">
       {/* Profile */}
-      <button className="flex items-center justify-center w-8 h-full hover:text-[#404040]">
+      <button className="hidden sm:flex items-center justify-center w-8 h-full hover:text-[#404040]">
         <User size={14} />
       </button>
       {/* Layout toggles */}
-      <button className="flex items-center justify-center w-8 h-full hover:text-[#404040]">
+      <button className="hidden sm:flex items-center justify-center w-8 h-full hover:text-[#404040]">
         <Columns size={14} />
       </button>
 
